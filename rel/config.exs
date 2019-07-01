8cb0ef4ec8123cd9c60deec520958f3363fd79c7
bryan <bryan.hunt@erlang-solutions.com>
2019-07-01 14:58:57 +0100

    have to set unused cookie in rel/conf + deleted dev

--- rel/config.exs
+++ rel/config.exs
@@ -22,18 +22,6 @@ use Distillery.Releases.Config,
 # when building in that environment, this combination of release
 # and environment configuration is called a profile
 
-environment :dev do
-  # If you are running Phoenix, you should make sure that
-  # server: true is set and the code reloader is disabled,
-  # even in dev mode.
-  # It is recommended that you build with MIX_ENV=prod and pass
-  # the --env flag to Distillery explicitly if you want to use
-  # dev mode.
-  set dev_mode: true
-  set include_erts: true
-  set cookie: :"4rO2[IVD5*T_dVGRwCPa/y$H0uT7:nxwhkIi4N5L}`MBFk$$!ec[kKcwDeU*XDuV"
-end
-
 environment :prod do
   set include_erts: false
   set include_src: false

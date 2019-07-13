65c1dc9dd21eb6b46655663972981b1d131fa5d0
bryan <bryan.hunt@erlang-solutions.com>
2019-07-13 20:54:02 +0100

    release 0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule IsItUp.Mixfile do
   def project do
     [
       app: :is_it_up,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
